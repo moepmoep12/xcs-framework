5e4f04025c4fffaf13778bf5afd1e30e22afd6c5
Andreas Schmidt <moepmoep12@gmail.com>
2021-06-15 12:08:01 +0200

    Started implementation of XCS algorithm

--- python/xcs/algorithm.py
+++ python/xcs/algorithm.py
@@ -1,2 +1,79 @@
-class XCS(object):
-    pass
+from typing import TypeVar, Generic
+
+from xcs.state import State
+from xcs.classifier_sets import Population
+from xcs.components import *
+
+# The data type for symbols
+SymbolType = TypeVar('SymbolType')
+# The data type for actions
+ActionType = TypeVar('ActionType')
+
+
+class XCS(Generic[SymbolType, ActionType]):
+
+    def __init__(self,
+                 performance_component: IPerformanceComponent,
+                 discovery_component: IDiscoveryComponent,
+                 learning_component: ILearningComponent):
+
+        if not isinstance(performance_component, IPerformanceComponent):
+            raise ValueError(f"{performance_component} is not of type {type(IPerformanceComponent)}")
+        if not isinstance(discovery_component, IDiscoveryComponent):
+            raise ValueError(f"{discovery_component} is not of type {type(IDiscoveryComponent)}")
+        if not isinstance(learning_component, ILearningComponent):
+            raise ValueError(f"{learning_component} is not of type {type(ILearningComponent)}")
+
+        self._performance_component: IPerformanceComponent = performance_component
+        self._discovery_component: IDiscoveryComponent = discovery_component
+        self._learning_component: ILearningComponent = learning_component
+        self._population: Population[SymbolType, ActionType] = None
+
+    def explore(self, state: State[SymbolType, ActionType]) -> ActionType:
+        """
+        Performs an iteration of the XCS algorithm in explore mode.
+        Exploring means discovering new behaviour instead of exploiting the current knowledge.
+        :param state: The current state.
+        :return: Returns the chosen action.
+        """
+        match_set = self._performance_component.generate_match_set(self._population, state)
+        if len(match_set) == 0:
+            # TO-DO: COVERING
+            pass
+
+        action: ActionType = self._performance_component.choose_action(match_set)
+
+        pass
+
+    @property
+    def performance_component(self) -> IPerformanceComponent:
+        return self._performance_component
+
+    @performance_component.setter
+    def performance_component(self, value: IPerformanceComponent):
+        if not isinstance(value, IPerformanceComponent):
+            raise ValueError(f"{value} is not of type {type(IPerformanceComponent)}")
+
+        self._performance_component = value
+
+    @property
+    def discovery_component(self) -> IDiscoveryComponent:
+        return self._discovery_component
+
+    @discovery_component.setter
+    def discovery_component(self, value: IDiscoveryComponent):
+        if not isinstance(value, IDiscoveryComponent):
+            raise ValueError(f"{value} is not of type {type(IDiscoveryComponent)}")
+
+        self._discovery_component = value
+
+    @property
+    def learning_component(self) -> ILearningComponent:
+        return self._learning_component
+
+    @learning_component.setter
+    def learning_component(self, value: ILearningComponent):
+        if not isinstance(value, ILearningComponent):
+            raise ValueError(f"{value} is not of type {type(ILearningComponent)}")
+
+        self._learning_component = value
